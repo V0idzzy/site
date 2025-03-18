62c733609e0b7406abce37682e632a96fa92022f
V0idzzy <v0idzy@proton.me>
2025-03-17 23:10:14 -0400

    Brand New Website :3
    I know I am managing this codebase very poorly. I will totally fix it up
    starting now like a new years resolution... totallyyyy...

--- js/fanart.js
+++ js/fanart.js
@@ -20,16 +20,14 @@ fetch("data/fanart.json")
 let AddFanArts = (data) => {
     let fanart = document.querySelector(".fanart-container");
     data.forEach(art => {
-        console.log(art);
         let newArt = document.createElement('a');
-        newArt.setAttribute("class", "card");
+        newArt.setAttribute("class", "fanart-card");
         newArt.setAttribute("href", `https://twitter.com/${art.username}`);
+        newArt.setAttribute("alt", `Fan Art from ${art.type} credit: ${art.username}`)
         newArt.setAttribute("target", "_blank");
         newArt.innerHTML =
             `
-                <h3 class="art-type">${art.type}</h3>
                 <img class="art" src="${art.path}">
-                <span class="artist-username">${art.username}</span>
             `;
         fanart.appendChild(newArt);
     });
