9bfec28bb8784d30e5c3c7721e698cb0c6773f95
V0idzzy <v0idzy@proton.me>
2025-05-05 18:33:41 -0400

    added banner images to meta tag

---
+++ js/supporters.js
@@ -0,0 +1,34 @@
+//generate supporter quotes divs 
+//show a line of text or change color if winner
+
+fetch("data/supporters.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('File not found');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        AddFanArts(data);
+    })
+    .catch(error => {
+        console.error('fetch error: ', error);
+    })
+
+
+let AddFanArts = (data) => {
+    let fanart = document.querySelector(".quote-container");
+    data.forEach(art => {
+        let newArt = document.createElement('a');
+        newArt.setAttribute("class", "fanart-card");
+        newArt.setAttribute("href", `https://twitter.com/${art.username}`);
+        newArt.setAttribute("alt", `Fan Art from ${art.type} credit: ${art.username}`)
+        newArt.setAttribute("target", "_blank");
+        newArt.innerHTML =
+            `
+                <img class="art" src="${art.path}">
+            `;
+        fanart.appendChild(newArt);
+    });
+}
